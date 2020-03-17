c376c674be6670ec2d9e3bf9cbdc4a79a7535e11
Captain Mcboatface <areeb7676@gmail.com>
2020-03-18 01:55:31 +0500

    new stuff

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -37,25 +37,96 @@ class _HomeState extends State<Home> {
                 flexibleSpace: Container(
                     decoration: BoxDecoration(
                         gradient: LinearGradient(colors: gradient)))),
-            body: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: <Widget>[
-                  Container(
-                      color: Color.fromRGBO(217, 217, 217, 1.0),
-                      padding:
-                          EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-                      child: Text(
-                        'Complete All Tests to Measure Safety of your Pool',
-                        style: GoogleFonts.poppins(
-                            textStyle: TextStyle(
-                                color: Color.fromRGBO(166, 166, 166, 1.0),
-                                fontSize: 20)),
-                        // style: ,
-                      )),
-                  Expanded(child: Container())
-                ])));
+            body: Container(
+              // margin: EdgeInsets.symmetric(vertical:20),
+              // padding: EdgeInsets.symmetric(vertical:20),
+              alignment: Alignment.center,
+              child: ListView(
+                physics: ClampingScrollPhysics(),
+                children: homeScreenCards(),
+              ),
+            )));
     // Container(
     //   child: TestScreen(uid:widget.uid),
     // );
+  }
+
+  List<Widget> homeScreenCards() {
+    List<Widget> jobCategoriesCards = [];
+
+    List<Widget> rows = [];
+    var jobCategories = [
+      {"name":"Run Test",
+        // "image" : 
+      },
+      {"name":"Previous Results"},
+      {"name":"Your Information"},
+      {"name":"Settings"}
+    ];
+    int i = 0;
+    for (var category in jobCategories) {
+      if (i < 2) {
+        rows.add(getCategoryContainer(category["name"]));
+        i++;
+      } else {
+        i = 0;
+        jobCategoriesCards.add(new Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: rows,
+        ));
+        rows = [];
+        rows.add(getCategoryContainer(category["name"]));
+        i++;
+      }
+    }
+    if (rows.length > 0) {
+      jobCategoriesCards.add(new Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: rows,
+      ));
+    }
+    return jobCategoriesCards;
+  }
+
+  Widget getCategoryContainer(String categoryName) {
+    return new Container(
+        margin: EdgeInsets.only(right: 10, left: 10, top: 20),
+        height: 160,
+        width: 140,
+        //
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.all(Radius.circular(15)),
+          boxShadow: [
+            new BoxShadow(
+              color: Colors.grey,
+              blurRadius: 10.0,
+            ),
+          ],
+        ),
+        child: FlatButton(
+          child: Container(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              // crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Icon
+                (Icons.info,size: 60,
+                  color: mainThemeColor,
+                ),
+                SizedBox(height: 10,)
+                ,
+                Text(categoryName),
+                
+              ],
+            ),
+          ),
+          onPressed: () {},
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(15.0),
+            
+          ),
+          
+        ));
   }
 }
